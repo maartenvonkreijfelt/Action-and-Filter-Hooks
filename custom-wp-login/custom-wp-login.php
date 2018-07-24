a6c7be0d67269bdab919c5b1d942e4b69923c914
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2018-07-24 18:05:47 +0200

    Update

--- custom-wp-login/custom-wp-login.php
+++ custom-wp-login/custom-wp-login.php
@@ -7,3 +7,11 @@
  * License: GPL2+
  * Text Domain: cwpl
  */
+
+add_action( 'login_enqueue_scripts', 'cwpl_login_stylesheet' );
+/**
+ * Load custom stylesheet on login page.
+ */
+function cwpl_login_stylesheet() {
+	wp_enqueue_style( 'cwpl-custom-stylsheet', plugin_dir_url(__FILE__) . 'login-styles.css' );
+}
