611c85cbb215eb5484bc2e04b097fc5bca743992
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2018-07-24 18:06:58 +0200

    Update

--- custom-wp-login/custom-wp-login.php
+++ custom-wp-login/custom-wp-login.php
@@ -15,3 +15,11 @@ add_action( 'login_enqueue_scripts', 'cwpl_login_stylesheet' );
 function cwpl_login_stylesheet() {
 	wp_enqueue_style( 'cwpl-custom-stylsheet', plugin_dir_url(__FILE__) . 'login-styles.css' );
 }
+
+add_filter( 'login_errors', 'cwpl_error_message');
+/**
+ * Returns a custom login error message.
+ */
+function cwpl_error_message() {
+	return 'Well, that was not it!';
+}
