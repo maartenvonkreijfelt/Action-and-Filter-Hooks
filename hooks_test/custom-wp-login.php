0baf811d8c9259a1aeebfa7e07bf5ab0493d1ae3
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2018-07-24 17:54:20 +0200

    Initial commit

---
+++ hooks_test/custom-wp-login.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Plugin Name: Custom WP Login
+ * Version: 0.1
+ * Author: Carrie Dils
+ * License: GPL2+
+ * Text Domain: cwpl
+ */
+
+add_action( 'login_enqueue_scripts', 'cwpl_login_stylesheet' );
+/**
+ * Load custom stylesheet on login page.
+ */
+function cwpl_login_stylesheet() {
+	wp_enqueue_style( 'cwpl-custom-stylsheet', plugin_dir_url(__FILE__) . 'login-styles.css' );
+}
+
+add_filter( 'login_errors', 'cwpl_error_message');
+/**
+ * Returns a custom login error message.
+ */
+function cwpl_error_message() {
+	return 'Well, that was not it!';
+}
